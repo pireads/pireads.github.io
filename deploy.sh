48d034506f5967fcb6ca888680ac526f72de5246
Avi <pireads@gmail.com>
2020-08-14 18:28:12 -0700

    push to prod

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ gatsby build
 echo avimoskoff.me > public/CNAME
 cp README.md public/README.md
 echo ".cache/*\nnode_modules/*" > public/.gitignore
-gh-pages -d public -b master
+gh-pages -d public -b prod
